057ae6a90776159541fa997d38fd2b6bc711a3c8
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-05-29 12:25:22 +1200

    awesome commit  mesasgeas

--- app/__init__.py
+++ app/__init__.py
@@ -33,8 +33,10 @@ def connect_db():
 #-----------------------------------------------------------
 @app.get("/")
 def home():
+    client = connect_db()
+    result = client.execute("SELECT * FROM things")
     return render_template("pages/home.jinja")
-
+    print(result.rows)
 
 #-----------------------------------------------------------
 # Thing details page
