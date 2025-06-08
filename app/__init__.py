db6333e25632b5e7d4016b340c766bdd11e9ebb8
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-06-09 10:54:43 +1200

    thing pages working with price

--- app/__init__.py
+++ app/__init__.py
@@ -31,16 +31,21 @@ def connect_db():
 @app.get("/")
 def home():
     client = connect_db()
-    result = client.execute("SELECT * FROM things")
-    return render_template("pages/home.jinja")
-    print(result.rows)
+    result = client.execute("SELECT id, name FROM things")
+    things = result.rows
+    return render_template("pages/home.jinja", things=things)
 
 #-----------------------------------------------------------
 # Thing details page
 #-----------------------------------------------------------
 @app.get("/thing/<int:id>")
 def show_thing(id):
-    return render_template("pages/thing.jinja")
+    client = connect_db()
+    sql = "SELECT id, name, price FROM things where id = ?"
+    values = [id]
+    result = client.execute(sql, values)
+    things = result.rows[0]
+    return render_template("pages/thing.jinja", thing=things)
 
 #-----------------------------------------------------------
 # New thing form page
